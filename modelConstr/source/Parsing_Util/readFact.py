7d4af3f4ec896d2ec0b199ed2a780bffb591974b
Liu Liu <niuye8911@gmail.com>
2018-04-09 17:34:16 -0400

    small fix

--- modelConstr/source/Parsing_Util/readFact.py
+++ modelConstr/source/Parsing_Util/readFact.py
@@ -1,2 +1,19 @@
 #read in a fact and generate a dictionary where the key is a config set, and the value is the cost
 def readFact(fact):
+    final_profile = {}
+    for line in fact:
+        col = line.split(',')
+        knob_name = ""
+        knob_val = 0.0
+        config_name = ""
+        cost = 0.0
+        for i in range(len(col)):
+            if i==len(col)-1:
+                cost = float(col[i])
+                continue
+            knob_name = col[i]
+            knob_val = float(col[i+1])
+            config_name+=knob_name+","+str(knob_val)
+            i+=1
+        final_profile[config_name] = cost
+    return final_profile
