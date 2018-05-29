ac8b0c756280e856f1067954392ab72f08dd7678
Liu Liu <niuye8911@gmail.com>
2018-05-29 13:58:36 -0400

    remove old files

--- modelConstr/source/runApp.py
+++
@@ -1,8 +0,0 @@
-from os import system
-def runSwap():
-    budget = range(8,80,8)
-    print budget
-    for i in budget:
-        system("bodytrack sequenceB_261 4 261 5000 4 0 1 -rsdg -cont -xml bodytrack_linear.xml -u 26 -b "+str(i))
-        system("mv output.txt outputs/output_l_"+str(i)+".txt")
-runSwap()
