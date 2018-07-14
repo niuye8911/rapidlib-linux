5a45ee366832595fa2553578a62507d6c620c9eb
Liu Liu <niuye8911@gmail.com>
2018-07-14 16:15:54 -0700

    CPP source refactor

--- cppSource/RSDG.h
+++ cppSource/RSDG.h
@@ -1,35 +1,34 @@
-#include <vector>
-#include <string>
 #include <map>
+#include <string>
 #include <tuple>
+#include <vector>
 
 using namespace std;
 
 // List formating
-#define START_CHAR	'S'
-#define DELIM_CHAR	'_'
+#define START_CHAR 'S'
+#define DELIM_CHAR '_'
 
-// Easy way to get at each part of the address index_t triplet 
-#define INDEX_TOP(idx)		get<0>(idx)
-#define INDEX_LEVEL(idx)	get<1>(idx)
-#define INDEX_BASIC(idx)	get<2>(idx)
+// Easy way to get at each part of the address index_t triplet
+#define INDEX_TOP(idx) get<0>(idx)
+#define INDEX_LEVEL(idx) get<1>(idx)
+#define INDEX_BASIC(idx) get<2>(idx)
 
 // Get edge_t and xml_edge_t fields
-#define EDGE_VALUE(edge)	get<0>(edge)	// mission value / weight
-#define EDGE_COST(edge)		get<1>(edge)	// energy cost
-#define EDGE_INDEX(edge)	get<2>(edge)	// Scheme edge has index
-#define EDGE_NAME(edge)		get<2>(edge)	// XML has string
+#define EDGE_VALUE(edge) get<0>(edge) // mission value / weight
+#define EDGE_COST(edge) get<1>(edge)  // energy cost
+#define EDGE_INDEX(edge) get<2>(edge) // Scheme edge has index
+#define EDGE_NAME(edge) get<2>(edge)  // XML has string
 
 // Sets all 3 values of the index_t triple to 0
-#define ZERO_OUT_IDX_T(idx)		\
-		get<0>(idx) = 0;		\
-		get<1>(idx) = 0;		\
-		get<2>(idx) = 0;
+#define ZERO_OUT_IDX_T(idx)                                                    \
+  get<0>(idx) = 0;                                                             \
+  get<1>(idx) = 0;                                                             \
+  get<2>(idx) = 0;
 
-// Check's if string is a node representation 
-#define IS_NODE(str)							\
-		str.find(START_CHAR) != string::npos	\
-		&& str.find(DELIM_CHAR) != string::npos 
+// Check's if string is a node representation
+#define IS_NODE(str)                                                           \
+  str.find(START_CHAR) != string::npos &&str.find(DELIM_CHAR) != string::npos
 
 #define MIN 0
 #define MAX 1
@@ -40,13 +39,14 @@ using namespace std;
 #define INT_MAX 99999
 #endif
 
-// top | level | basic - unsigned shorts to fit in an 8 byte hashed value for map key
-typedef tuple<unsigned short,unsigned short, unsigned short> index_t;
+// top | level | basic - unsigned shorts to fit in an 8 byte hashed value for
+// map key
+typedef tuple<unsigned short, unsigned short, unsigned short> index_t;
 
 // value | cost | index_t = address (source of dependency
-typedef tuple<float, float, index_t > edge_t;	
+typedef tuple<float, float, index_t> edge_t;
 
-// XML version of edge_t  address is a string 
+// XML version of edge_t  address is a string
 typedef tuple<float, float, string> xml_edge_t;
 
 // Continuous edge <sinkmin, sinkmax, source, sourcemin, sourcemax>
@@ -55,151 +55,152 @@ typedef tuple<float, float, string, float, float> cont_edge_t;
 extern bool DEBUG_MSGS, SUPPRESS_ERRORS;
 
 // Base class for Top, Level, and Basic nodes
-class Node 
-{
-    private:
-        vector<vector<edge_t>> edges;			// Scheme
-        vector<vector<xml_edge_t>> xml_edges;	// XML
-	vector<vector<xml_edge_t>> xml_outedges;
-	vector<vector<cont_edge_t>> cont_edges;
-        index_t address;
-        string name;
-        bool basic, level, top;
-    public:
-        vector<vector<edge_t>> *getSchemeEdges(void);
-        vector<vector<xml_edge_t>> *getXMLEdges(void);
-	vector<vector<cont_edge_t>> *getContEdges(void);
-	vector<vector<xml_edge_t>> *getXMLOutEdges(void);
-        index_t *getAddr(void);
-        string getName();
-        void setEdgeWeight(index_t, float value = 0, float cost = 0);		// Might not need cost default = 0
-        void addEdges(vector<edge_t>);
-	void addContEdges(vector<cont_edge_t>);
-        void addEdges(vector<xml_edge_t>);
-	void addOutEdges(vector<xml_edge_t>);
-        void setAddr(index_t *);	// Scheme
-        void setType(int type);		// 0 = top , 1 = level, 2 = basic
-        void setName(string name);
-        bool isBasic(void);
-        bool isLevel(void);
-        bool isTop(void);
+class Node {
+private:
+  vector<vector<edge_t>> edges;         // Scheme
+  vector<vector<xml_edge_t>> xml_edges; // XML
+  vector<vector<xml_edge_t>> xml_outedges;
+  vector<vector<cont_edge_t>> cont_edges;
+  index_t address;
+  string name;
+  bool basic, level, top;
+
+public:
+  vector<vector<edge_t>> *getSchemeEdges(void);
+  vector<vector<xml_edge_t>> *getXMLEdges(void);
+  vector<vector<cont_edge_t>> *getContEdges(void);
+  vector<vector<xml_edge_t>> *getXMLOutEdges(void);
+  index_t *getAddr(void);
+  string getName();
+  void setEdgeWeight(index_t, float value = 0,
+                     float cost = 0); // Might not need cost default = 0
+  void addEdges(vector<edge_t>);
+  void addContEdges(vector<cont_edge_t>);
+  void addEdges(vector<xml_edge_t>);
+  void addOutEdges(vector<xml_edge_t>);
+  void setAddr(index_t *); // Scheme
+  void setType(int type);  // 0 = top , 1 = level, 2 = basic
+  void setName(string name);
+  bool isBasic(void);
+  bool isLevel(void);
+  bool isTop(void);
 };
 
 // Basic nodes - represent redundant implementations
-class Basic : public Node 
-{	
-	private:
-		double value;
-		float cost;
-		double costo2;
-		double costo1;
-		double costc;
-		double mvo2;
-		double mvo1;
-		double mvc;
-		double maxValue = INT_MAX;
-		double minValue = INT_MIN;
-		bool CONTINUOUS = false;
-		bool MINMAX = false;
-		bool PIECEWISE = false;
-		map<string, float> coeffs;
-		map<string, float> mvcoeffs;
-	public:
-		map<string, vector<float>> piececoeffs;
-                map<string, vector<float>> piecemvcoeffs;
-		map<string, pair<float,float>> segments;
-                map<string, pair<float,float>> segment_costvalues;        
-                map<string,pair<float,float>> segment_mvvalues;
-		void setValue(double );
-		void setValueOrder(double, double, double);
-		void setCost(float);
-		void setCostOrder(double, double, double);
-		double getValue(void);
-		float getCost(void);
-		bool isContinuous();
-		bool isPieceWise();
-		void setContinuous();
-		void setPieceWise();
-		void addSegment(string,float,float,float,float,bool);
-		void getCostOrder(vector<double>&);
-		void getValueOrder(vector<double>&);
-		bool hasMinMax();
-		void setContMax(double);
-		void setContMin(double);
-		double getMaxValue();
-		double getMinValue();
-		void addContCoeff(string, float);
-		void addContMVCoeff(string, float);
-		void addContPieceCoeff(string, float, float, float, bool);
-		map<string, float> &getCoeffs();
-		map<string, float> &getMVCoeffs();
-		Basic(index_t *, string = "");		// Scheme
-		Basic(string);			// XML
+class Basic : public Node {
+private:
+  double value;
+  float cost;
+  double costo2;
+  double costo1;
+  double costc;
+  double mvo2;
+  double mvo1;
+  double mvc;
+  double maxValue = INT_MAX;
+  double minValue = INT_MIN;
+  bool CONTINUOUS = false;
+  bool MINMAX = false;
+  bool PIECEWISE = false;
+  map<string, float> coeffs;
+  map<string, float> mvcoeffs;
+
+public:
+  map<string, vector<float>> piececoeffs;
+  map<string, vector<float>> piecemvcoeffs;
+  map<string, pair<float, float>> segments;
+  map<string, pair<float, float>> segment_costvalues;
+  map<string, pair<float, float>> segment_mvvalues;
+  void setValue(double);
+  void setValueOrder(double, double, double);
+  void setCost(float);
+  void setCostOrder(double, double, double);
+  double getValue(void);
+  float getCost(void);
+  bool isContinuous();
+  bool isPieceWise();
+  void setContinuous();
+  void setPieceWise();
+  void addSegment(string, float, float, float, float, bool);
+  void getCostOrder(vector<double> &);
+  void getValueOrder(vector<double> &);
+  bool hasMinMax();
+  void setContMax(double);
+  void setContMin(double);
+  double getMaxValue();
+  double getMinValue();
+  void addContCoeff(string, float);
+  void addContMVCoeff(string, float);
+  void addContPieceCoeff(string, float, float, float, bool);
+  map<string, float> &getCoeffs();
+  map<string, float> &getMVCoeffs();
+  Basic(index_t *, string = ""); // Scheme
+  Basic(string);                 // XML
 };
 
-// Level nodes (container for a group of basic nodes) - represent functions of a service 
-class Level : public Node 
-{
-	private:
-		int level;		// XML level number
-		vector<Basic *> basic_nodes;	
-	public:
-		vector<Basic *> *getBasicNodes(void);
-		void addBasicNode(Basic *);
-		int getLevelNum();
-		Level(index_t *);	// Scheme
-		Level(string);			// XML
-		~Level();
+// Level nodes (container for a group of basic nodes) - represent functions of a
+// service
+class Level : public Node {
+private:
+  int level; // XML level number
+  vector<Basic *> basic_nodes;
+
+public:
+  vector<Basic *> *getBasicNodes(void);
+  void addBasicNode(Basic *);
+  int getLevelNum();
+  Level(index_t *); // Scheme
+  Level(string);    // XML
+  ~Level();
 };
 
-// Top nodes - represent a single service 
-class Top : public Node
-{
-	private:
-		vector<Level *> level_nodes;	// XML data structure
-	public:
-		vector<Level *> *getLevelNodes(void);
-		void addLevelNode(Level *);
-		Top(index_t *, string = "");	// Scheme
-		Top(string);					// XML
-		~Top();
+// Top nodes - represent a single service
+class Top : public Node {
+private:
+  vector<Level *> level_nodes; // XML data structure
+public:
+  vector<Level *> *getLevelNodes(void);
+  void addLevelNode(Level *);
+  Top(index_t *, string = ""); // Scheme
+  Top(string);                 // XML
+  ~Top();
 };
 
 // Main Data Structure for both XML and Scheme List representations of RSDG
-class RSDG
-{
-	private:
-		double	budget;
-		long long hashIndex(index_t *);
-		map<long long, Node *> rsdg;	// Scheme 	
-		vector<Top *> xml_rsdg;			// XML
-		vector<string> forceOn;
-		vector<string> forceOnTmp;
-		vector<string> forceOff;
-	public:
-		double  targetMV;
-		bool minmax = true;
-		Node* getNodeFromIndex(index_t *);		// Scheme 
-		Node* getNodeFromName(string);			// XML
-		void parseSchemeList(string infile);	// parse Scheme List
-		void parseXML(string infile);			// parse XML 
-		void addNode(index_t *);				// Scheme format
-		void addNode(string);					// XML format  
-		void writeSchemeLp(string);				// lp from Scheme 
-		void writeXMLLp(string,bool);				// lp from XML
-		void setBudget(double);					// Set energy budget
-		bool getSolution(void);					// Solve using heuristics
-		void updateMissionValue(string serviceName, int value, bool exp);
-		void updateCost(string name, double cost);
-		void updateEdgeCost(string, string, double);
-		void addOn(string);
-		void addTmpOn(string);
-		void resetTmpOn();
-		void addOff(string);
-		vector<string> getOn();
-		vector<string> getOff();
+class RSDG {
+private:
+  double budget;
+  long long hashIndex(index_t *);
+  map<long long, Node *> rsdg; // Scheme
+  vector<Top *> xml_rsdg;      // XML
+  vector<string> forceOn;
+  vector<string> forceOnTmp;
+  vector<string> forceOff;
 
-		~RSDG();
+public:
+  double targetMV;
+  bool minmax = true;
+  Node *getNodeFromIndex(index_t *);   // Scheme
+  Node *getNodeFromName(string);       // XML
+  void parseSchemeList(string infile); // parse Scheme List
+  void parseXML(string infile);        // parse XML
+  void addNode(index_t *);             // Scheme format
+  void addNode(string);                // XML format
+  void writeSchemeLp(string);          // lp from Scheme
+  void writeXMLLp(string, bool);       // lp from XML
+  void setBudget(double);              // Set energy budget
+  bool getSolution(void);              // Solve using heuristics
+  void updateMissionValue(string serviceName, int value, bool exp);
+  void updateCost(string name, double cost);
+  void updateEdgeCost(string, string, double);
+  void addOn(string);
+  void addTmpOn(string);
+  void resetTmpOn();
+  void addOff(string);
+  vector<string> getOn();
+  vector<string> getOff();
+
+  ~RSDG();
 };
 
-RSDG* rsdgGen(string);
+RSDG *rsdgGen(string);
--- cppSource/dynamic.cpp
+++ cppSource/dynamic.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
-#include <unistd.h>
-#include <sstream>
 #include <fstream>
+#include <iostream>
 #include <map>
-#include <vector>
-#include <utility>
 #include <math.h>
+#include <sstream>
 #include <string>
+#include <unistd.h>
+#include <utility>
+#include <vector>
 
 #define END ";"
 using namespace std;
@@ -14,143 +14,159 @@ using namespace std;
 ofstream report;
 ifstream f;
 ifstream g;
-map<string,vector<int>> fact;
-map<string,vector<double>> guess;
-vector<pair<string,string>> ctrs;
+map<string, vector<int>> fact;
+map<string, vector<double>> guess;
+vector<pair<string, string>> ctrs;
 vector<string> bounds;
 
 int totNum;
 double maxerr;
-void readObserved(){
-	ifstream constraints;
-	constraints.open("observed");
-	while(!constraints.eof()){
-		string ctr1,ctr2;
-		getline(constraints,ctr1);
-		if(ctr1=="")getline(constraints,ctr1);
-		getline(constraints,ctr2);
-		ctrs.push_back(make_pair(ctr1,ctr2));
-	}
+void readObserved() {
+  ifstream constraints;
+  constraints.open("observed");
+  while (!constraints.eof()) {
+    string ctr1, ctr2;
+    getline(constraints, ctr1);
+    if (ctr1 == "")
+      getline(constraints, ctr1);
+    getline(constraints, ctr2);
+    ctrs.push_back(make_pair(ctr1, ctr2));
+  }
 }
 
-void genProblem(vector<int> &comb){
-	ifstream orig;
-	orig.open("factorig.lp");
-	ofstream prob;
-	prob.open("factgen.lp");
-	prob<<orig.rdbuf();
-	
-	for(int i = 0; i<comb.size(); i++){
-		prob<<ctrs[comb[i]].first<<endl;
-		prob<<ctrs[comb[i]].second<<endl;
-	}
-	ifstream end;
-	end.open("factend.lp");
-	prob<<end.rdbuf();
-	prob.close();		
-	string cmd="gurobi_cl ResultFile=max.sol ";
-	cmd+="factgen.lp";
-	system(cmd.c_str());
+void genProblem(vector<int> &comb) {
+  ifstream orig;
+  orig.open("factorig.lp");
+  ofstream prob;
+  prob.open("factgen.lp");
+  prob << orig.rdbuf();
+
+  for (int i = 0; i < comb.size(); i++) {
+    prob << ctrs[comb[i]].first << endl;
+    prob << ctrs[comb[i]].second << endl;
+  }
+  ifstream end;
+  end.open("factend.lp");
+  prob << end.rdbuf();
+  prob.close();
+  string cmd = "gurobi_cl ResultFile=max.sol ";
+  cmd += "factgen.lp";
+  system(cmd.c_str());
 }
 
-void readGuess(){
-	ifstream gg;
-	gg.open("max.sol");
-	string name;
-	int level;
-	string tmp;
-	while(!gg.eof()){
-		gg>>tmp;
-		level = tmp[tmp.size()-1]-'0'-1;
-		tmp = tmp.substr(0,tmp.size()-1);
-		if(fact.find(tmp)==fact.end())continue;
-		guess[tmp].resize(fact[tmp].size());
-		gg>>guess[tmp][level];
-	}
-	//add in specific known value
-/*	guess["GPS"].push_back(298);
-	guess["SYS"].push_back(375);
-	guess["comm"].push_back(109);*/
+void readGuess() {
+  ifstream gg;
+  gg.open("max.sol");
+  string name;
+  int level;
+  string tmp;
+  while (!gg.eof()) {
+    gg >> tmp;
+    level = tmp[tmp.size() - 1] - '0' - 1;
+    tmp = tmp.substr(0, tmp.size() - 1);
+    if (fact.find(tmp) == fact.end())
+      continue;
+    guess[tmp].resize(fact[tmp].size());
+    gg >> guess[tmp][level];
+  }
+  // add in specific known value
+  /*	guess["GPS"].push_back(298);
+          guess["SYS"].push_back(375);
+          guess["comm"].push_back(109);*/
 }
 
-void readFact(){
-	string name;
-	int level;
-	while(!f.eof()){
-		f>>name;
-		if(name=="#")break;
-		f>>level;
-		fact[name].resize(level);
-	}
+void readFact() {
+  string name;
+  int level;
+  while (!f.eof()) {
+    f >> name;
+    if (name == "#")
+      break;
+    f >> level;
+    fact[name].resize(level);
+  }
 }
 
-double checkRate(){
-	int num = 0;
-	double diff = 0;
-	maxerr = -1;
-	while(!f.eof()){
-		string s1,s2,s3,s4;
-		int l1,l2,l3,l4;
-		double v;
-		f>>s1;f>>l1;f>>s2;f>>l2;f>>s3;f>>l3;f>>s4;f>>l4;
-		f>>v;
-		if(s1=="")continue;
-		double gvalue = guess[s1][l1-1]+guess[s2][l2-1]+guess[s3][l3-1]+guess[s4][l4-1]+782;
-		if(l3>3)gvalue-=298;
-		cout<<guess[s1][l1-1]<<" "<<guess[s2][l2-1]<<" "<<guess[s3][l3-1]<<" "<<guess[s4][l4-1]<<" "<<gvalue<<"->"<<v<<endl;
-		double dif = (gvalue-v)/v;
-		dif=dif>0?dif:-dif;
-		diff += dif;
-		maxerr = max(maxerr,dif);
-		num++;
-	}	
-	totNum = num;
-	double errC = diff/num;
-	return errC;
+double checkRate() {
+  int num = 0;
+  double diff = 0;
+  maxerr = -1;
+  while (!f.eof()) {
+    string s1, s2, s3, s4;
+    int l1, l2, l3, l4;
+    double v;
+    f >> s1;
+    f >> l1;
+    f >> s2;
+    f >> l2;
+    f >> s3;
+    f >> l3;
+    f >> s4;
+    f >> l4;
+    f >> v;
+    if (s1 == "")
+      continue;
+    double gvalue = guess[s1][l1 - 1] + guess[s2][l2 - 1] + guess[s3][l3 - 1] +
+                    guess[s4][l4 - 1] + 782;
+    if (l3 > 3)
+      gvalue -= 298;
+    cout << guess[s1][l1 - 1] << " " << guess[s2][l2 - 1] << " "
+         << guess[s3][l3 - 1] << " " << guess[s4][l4 - 1] << " " << gvalue
+         << "->" << v << endl;
+    double dif = (gvalue - v) / v;
+    dif = dif > 0 ? dif : -dif;
+    diff += dif;
+    maxerr = max(maxerr, dif);
+    num++;
+  }
+  totNum = num;
+  double errC = diff / num;
+  return errC;
 }
 
+int main() {
+  report.open("report");
+  g.open("max.sol");
+  readObserved();
+  vector<int> repSet;
+  repSet.push_back(0);
+  double errtot = 99999999;
+  int count = 1;
+  map<int, bool> visited;
 
-int main(){
-	report.open("report");
-	g.open("max.sol");
-	readObserved();
-	vector<int> repSet;
-	repSet.push_back(0);
-	double errtot=99999999;
-	int count = 1;
-	map<int,bool> visited;
-	
-	while(errtot>=0.1 || maxerr>=0.15){
-		double errcur=999999;
-		int repcur = -1;
-		for(int i = 1;i<ctrs.size(); i++){
-			if(visited.find(i)==visited.end())
-				repSet.push_back(i);
-			else continue;
-			f.open("fact");
-			readFact();
-			genProblem(repSet);
-			readGuess();
-			double errtmp=checkRate();
-			if(errtmp<errcur){
-				repcur=i;
-				errcur = errtmp;
-			}
-			f.close();
-			repSet.pop_back();
-		}
-		if(repcur==-1)break;
-		repSet.push_back(repcur);
-		errtot=errcur;
-		visited[repcur]=true;
-		count++;
-		cout<<"added"<<repcur<<endl;
-		report<<"added "<<repcur<<"->"<<errtot<<"max:"<<maxerr<<endl;
-	}
-	cout<<"With error = "<<errtot<<" in "<<totNum<<" combinations"<<endl;
-	cout<<"Max error = "<<maxerr<<endl;
-	cout<<"count="<<count<<endl;
-	report.close();	
-	return 1;
-
+  while (errtot >= 0.1 || maxerr >= 0.15) {
+    double errcur = 999999;
+    int repcur = -1;
+    for (int i = 1; i < ctrs.size(); i++) {
+      if (visited.find(i) == visited.end())
+        repSet.push_back(i);
+      else
+        continue;
+      f.open("fact");
+      readFact();
+      genProblem(repSet);
+      readGuess();
+      double errtmp = checkRate();
+      if (errtmp < errcur) {
+        repcur = i;
+        errcur = errtmp;
+      }
+      f.close();
+      repSet.pop_back();
+    }
+    if (repcur == -1)
+      break;
+    repSet.push_back(repcur);
+    errtot = errcur;
+    visited[repcur] = true;
+    count++;
+    cout << "added" << repcur << endl;
+    report << "added " << repcur << "->" << errtot << "max:" << maxerr << endl;
+  }
+  cout << "With error = " << errtot << " in " << totNum << " combinations"
+       << endl;
+  cout << "Max error = " << maxerr << endl;
+  cout << "count=" << count << endl;
+  report.close();
+  return 1;
 }
