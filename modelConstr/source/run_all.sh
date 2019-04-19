571d2be4d6e1aeb44226670c6c6cbf4197a725aa
Liu Liu <niuye8911@gmail.com>
2019-04-19 14:36:59 -0400

    udpate script for data collecting

--- modelConstr/source/run_all.sh
+++ modelConstr/source/run_all.sh
@@ -2,3 +2,6 @@
 sudo python rapid.py -C ../data/nnPort/config_algae.json
 sudo python rapid.py -C ../data/svmPort/config_algae.json
 sudo python rapid.py -C ../data/facePort/config_algae.json
+sudo python rapid.py -C ../data/bodyPort/config_algae.json
+sudo python rapid.py -C ../data/ferretPort/config_algae.json
+sudo python rapid.py -C ../data/swapPort/config_algae.json
