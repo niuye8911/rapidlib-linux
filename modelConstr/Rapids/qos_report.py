376979b8f8f25b0ed80050dba003e26b113f82d9
Liu Liu <niuye8911@gmail.com>
2019-08-30 11:57:22 -0400

    qos_report

--- modelConstr/Rapids/qos_report.py
+++ modelConstr/Rapids/qos_report.py
@@ -24,7 +24,7 @@ def draw(qos_file, output):
                    marker={'color': colors[mode]},
                    x=apps,
                    y=list(sub_df['mean']),
-                   width=[0.15]*len(apps),
+                   width=[0.18]*len(apps),
                    error_y=dict(type='data',
                                 symmetric=False,
                                 array=list(sub_df['max'] - sub_df['mean']),
@@ -37,7 +37,7 @@ def draw(qos_file, output):
         title=go.layout.yaxis.Title(text="Normalized QoS", font=dict(
             size=18))),
                       barmode='group',
-                      bargap=0.55,
+                      bargap=0.45,
                       bargroupgap=1,
                       paper_bgcolor='white',
                       plot_bgcolor='white')
@@ -48,7 +48,7 @@ def draw(qos_file, output):
     fig.update_layout(legend=go.layout.Legend(
         x=0, y=1.1, traceorder="normal", font=dict(size=20, color="black")))
     fig.update_layout(legend_orientation='h')
-    fig.show()
+    fig.write_image(output)
 
 
 draw('./qos_report.csv', 'qos_report.png')
