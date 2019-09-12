39f2f0b27bfbd3826e19038be5d88a343fe6791e
liuliu <niuye8911@gmail.com>
2019-09-12 15:02:58 -0400

    budget run automation

--- modelConstr/Rapids/rapid.py
+++ modelConstr/Rapids/rapid.py
@@ -48,6 +48,7 @@ THRESHOLD = 0.05
 RS_THRESHOLD = 0.05
 
 RSRUN = False
+OVERHEAD_RUN_BUDGETS=[0.2,0.5,0.8]
 
 
 def main(argv):
@@ -355,16 +356,17 @@ def main(argv):
             # only run this for piecewise
             module = imp.load_source("", appInfo.METHODS_PATH)
             appMethods = module.appMethods(appInfo.APP_NAME, appInfo.OBJ_PATH)
-            report = appMethods.overheadMeasure()
-            output_name = './outputs/' + appname + "/overhead_report_" + appInfo.APP_NAME + ".csv"
-            columns = [
-                'Unit', 'MV', 'Augmented_MV','Budget','Exec_Time', 'OverBudget', 'RC_TIME','RC_NUM','SUCCESS','overhead_pctg'
-            ]
-            with open(output_name, 'w') as output:
-                writer = csv.DictWriter(output, fieldnames=columns)
-                writer.writeheader()
-                for data in report:
-                    writer.writerow(data)
+            for budget in OVERHEAD_RUN_BUDGETS:
+                report = appMethods.overheadMeasure(budget)
+                output_name = './outputs/' + appname + "/overhead_report_" + appInfo.APP_NAME + "_"+str(budget)+".csv"
+                columns = [
+                    'Unit', 'MV', 'Augmented_MV','Budget','Exec_Time', 'OverBudget', 'RC_TIME','RC_NUM','SUCCESS','overhead_pctg'
+                ]
+                with open(output_name, 'w') as output:
+                    writer = csv.DictWriter(output, fieldnames=columns)
+                    writer.writeheader()
+                    for data in report:
+                        writer.writerow(data)
 
     # ######SOME EXTRA MODES#############
 
